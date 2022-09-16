90bea9e55fb614144a07285f71c8aad6074b0545
Johnathan Guzman <45727071+johnathanthedev@users.noreply.github.com>
2022-09-15 23:35:49 -0500

    Ruby solution

---
+++ 14. Longest common prefix/solution.rb
@@ -0,0 +1,19 @@
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+    return "" if strs == []
+    common_pre = ""
+    short_str = (strs.sort!).delete_at(0)
+    short_str.each_char do |c|
+         strs.each do |str|
+            if(str.start_with?(common_pre + c))
+                next 
+            else
+                return common_pre
+            end
+         end
+        common_pre += c
+    end
+end
+
+longest_common_prefix(["flower","flow","flight"])
